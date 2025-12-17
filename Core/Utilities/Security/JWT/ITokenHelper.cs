c3a6df5e1fea42c91f67f8817e2ff5ab9c3e5cf1
İsmail Topuçar <168295963+ismailtpcrr@users.noreply.github.com>
2025-12-17 18:29:30 +0300

    son değişiklik

---
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,9 @@
+using System.Security.Claims;
+using Core.Entities.Concrede;
+
+namespace Core.Utilities.Security.JWT;
+
+public interface ITokenHelper
+{
+    AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+}
